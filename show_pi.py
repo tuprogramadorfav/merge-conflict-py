0dbc226980f677ee036b444e8135bc960f20f269
Martin Dreher <martin@thinkpad>
2020-02-26 12:39:25 +0100

    inline show_pi

--- show_pi.py
+++ show_pi.py
@@ -3,8 +3,5 @@
 def approximate_pi():
     return 355 / 113
 
-def show_pi():
-    print("pi is approximately {:.8f}".format(approximate_pi()))
-
 if __name__ == "__main__":
-    show_pi()
+    print("pi is approximately {:.8f}".format(approximate_pi()))
