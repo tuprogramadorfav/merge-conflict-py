9c946a7cd5015d135042473d83c07a873a12ecb8
Martin Dreher <martin@thinkpad>
2020-02-26 11:23:48 +0100

    make pi symbol unicode

--- show_pi.py
+++ show_pi.py
@@ -4,7 +4,7 @@ def approximate_pi():
     return 355 / 113
 
 def show_pi():
-    print("pi is {:.5f}".format(approximate_pi()))
+    print("π is {:.5f}".format(approximate_pi()))
 
 if __name__ == "__main__":
     show_pi()
