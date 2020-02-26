b0259144fd5573926d6b37fd25cc1bef78c6bc99
Martin Dreher <martin@thinkpad>
2020-02-26 11:21:30 +0100

    Initial version

---
+++ show_pi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def approximate_pi():
+    return 355 / 113
+
+def show_pi():
+    print("pi is {:.5f}".format(approximate_pi()))
+
+if __name__ == "__main__":
+    show_pi()
