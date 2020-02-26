d6498480cddcf53c0750a1e4eca7ee492f35d6ff
Martin Dreher <martin@thinkpad>
2020-02-26 11:25:13 +0100

    show more pi digits

--- show_pi.py
+++ show_pi.py
@@ -4,7 +4,7 @@ def approximate_pi():
     return 355 / 113
 
 def show_pi():
-    print("pi is {:.5f}".format(approximate_pi()))
+    print("pi is approximately {:.8f}".format(approximate_pi()))
 
 if __name__ == "__main__":
     show_pi()
